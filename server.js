c3f477ce1d51a7460b9ecfa0f03e17cbfa9dcdf9
Jota <damatojoel25@gmail.com>
2024-10-24 16:33:55 -0300

    c

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');  // Agregar cors
 const bancoRoutes = require('./routes/bancorout');
 const loginRoutes = require('./routes/loginrout');
 
 // Inicializamos la aplicación Express
 const app = express();
 const PORT = 3000;
+
+// Middleware para habilitar CORS
+app.use(cors()); // Esto permite las solicitudes desde cualquier origen
 
 // Middleware para analizar el cuerpo de las solicitudes
 app.use(bodyParser.json());
